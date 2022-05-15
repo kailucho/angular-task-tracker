589b7fd86a25dd59681e5768812ef693021a5e46
kaido <luijhy9234@gmail.com>
2022-05-15 01:56:49 -0500

    modify to work with local data

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -15,19 +15,22 @@ export class TasksComponent implements OnInit {
   constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
-    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks)
+    // this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks)
+    this.tasks = TASKS
   }
   deleteTask(task: Task) {
-    this.taskService.deleteTask(task).subscribe(() => this.tasks = this.tasks.filter(t => t.id !== task.id))
-
+    // this.taskService.deleteTask(task).subscribe(() => this.tasks = this.tasks.filter(t => t.id !== task.id))
+    this.tasks = this.tasks.filter(t => t.id !== task.id)
   }
-  toggleRemider(task: Task) {
+  toggleReminder(task: Task) {
     task.reminder = !task.reminder;
-    this.taskService.updateTaskRemidner(task).subscribe()
-
+    // this.taskService.updateTaskRemidner(task).subscribe()
+    let index = this.tasks.findIndex(x => x.id === task.id);
+    this.tasks[index] = task
   }
   addTask(task: Task) {
-    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
+    // this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
+    this.tasks.push(task)
   }
 
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs'
+import { Observable, of } from 'rxjs'
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 
 import { Task } from 'src/app/Task'
+import { TASKS } from 'src/app/mock-tasks'
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
